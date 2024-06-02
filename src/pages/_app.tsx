ae14a6f3a45dfa200512460a70a9af175ee8ae94
Eliezer Baschkier <eliezerbaschkier@gmail.com>
2024-06-02 19:06:17 -0300

    Install React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "antd/dist/reset.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+    HydrationBoundary,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const [queryClient] = useState(() => new QueryClient());
+    return (
+        <QueryClientProvider client={queryClient}>
+            <HydrationBoundary state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </HydrationBoundary>
+        </QueryClientProvider>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "antd";
+import { useQuery } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+    const { data } = useQuery({
+        queryKey: ["test"],
+        queryFn: () =>
+            fetch("http://jsonplaceholder.typicode.com/users").then((res) =>
+                res.json()
+            ),
+    });
+
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
